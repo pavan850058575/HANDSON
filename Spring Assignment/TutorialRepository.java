5b792b56ae6000c3631d27f19d37bb1b91779de3
pavan850058575 <gndpavan561@gmail.com>
2022-03-04 16:41:52 +0530

    Assignment

---
+++ Spring Assignment/TutorialRepository.java
@@ -0,0 +1,9 @@
+package com.org.datajpa.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.org.datajpa.model.Tutorial;
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+  List<Tutorial> findByPublished(boolean published);
+  List<Tutorial> findByTitleContaining(String title);
+}
